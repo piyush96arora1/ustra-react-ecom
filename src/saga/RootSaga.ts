967715de3ae806ed7dd45e55836180f446ed6790
unknown <piyush96arora@gmail.com>
2019-04-24 01:40:20 +0530

    corrected serice call

--- src/saga/RootSaga.ts
+++ src/saga/RootSaga.ts
@@ -27,11 +27,12 @@ export function* fetchCategoriesAsync() {
 
   export function* fetchProduct(payload:any) {
     try{
+      if(payload.payload!='backdropClick'){
     const {data}= yield call(UstraService.getProduct,payload);
     yield put({
       type: "FETCH_PRODUCT",
       payload: { products:data.products,id:payload.payload }
-    });
+    });}
   }
   catch(error)
   {
