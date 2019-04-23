3c35f47d3abb1794a2120cf5bd9b1b6a6935f81b
unknown <piyush96arora@gmail.com>
2019-04-23 19:41:18 +0530

    Updates

---
+++ precache-manifest.a1153c3b9a34c21a25611886640ffcd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ustra-react-ecom/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e06f4add87700037937c",
+    "url": "/ustra-react-ecom/static/js/runtime~main.f1f178d5.js"
+  },
+  {
+    "revision": "db740ba9a9d1239a0236",
+    "url": "/ustra-react-ecom/static/js/main.743938c6.chunk.js"
+  },
+  {
+    "revision": "e2fc72d746b0cbeefc29",
+    "url": "/ustra-react-ecom/static/js/2.2d8f95dc.chunk.js"
+  },
+  {
+    "revision": "db740ba9a9d1239a0236",
+    "url": "/ustra-react-ecom/static/css/main.b871943b.chunk.css"
+  },
+  {
+    "revision": "ce25f405b61751ce47d620cb447f56a8",
+    "url": "/ustra-react-ecom/index.html"
+  }
+];
